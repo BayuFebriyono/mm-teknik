c2362424a488875331506ba28e17df7604236162
BayuFebriyono <bayu.f.p.09@gmail.com>
2023-12-18 19:00:54 +0700

    add view admin and login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing_page');
 });
+
+Route::get('/tes', fn()=> view('admin.sidebar.main'));
+Route::get('/login', fn() => view('login'));
